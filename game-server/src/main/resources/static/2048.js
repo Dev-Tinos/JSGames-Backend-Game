bf63d0adf92a5d6961af958e529db282fa3dee07
KimChungO <kco796@naver.com>
2024-01-14 03:49:41 +0900

    [Fix]2048 userId 받기 테스트4

--- game-server/src/main/resources/static/2048.js
+++ game-server/src/main/resources/static/2048.js
@@ -14,6 +14,7 @@ var score;
 var userId = null;
 window.addEventListener("message", function (event) {
     document.getElementById("receivedData").innerHTML = event.data;
+    document.getElementById("receivedData").textContent = event.data;
     userId = event.data;
 });
 
